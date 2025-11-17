2eee8fcfabe537f9cb5ca2a2747f4118d8461933
monkey_d_luffy <andrialfiandi46@gmail.com>
2025-11-18 02:40:00 +0800

    first

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,11 +1,24 @@
 <?php
 $host = "p47bof.h.filess.io";
-$user = "iextra_treatedgas"; // default XAMPP
-$pass = "7ca2cd44bd50079fb3fa99fe769b8062153bdce4";     // kosongkan kalau belum diatur
-$db   = "iextra_treatedgas";
+$user = "iextra_treatedgas";
+$pass = "7ca2cd44bd50079fb3fa99fe769b8062153bdce4";
+$db   = "db_lextra";
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_init();
 
-if (!$koneksi) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+if (!mysqli_real_connect(
+    $koneksi,
+    $host,
+    $user,
+    $pass,
+    $db,
+    3307,
+    NULL,
+    MYSQLI_CLIENT_SSL
+)) {
+    die("Koneksi GAGAL: " . mysqli_connect_error());
 }
+
+echo "Koneksi BERHASIL pakai SSL!";
